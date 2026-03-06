75e8f5b650869109a9641614089d3d60c47b33f5
kgy <my@kaguya.org>
2026-03-06 15:27:58 +0800

    initial commit of quantumultx resource parser

---
+++ qx/resource_parser.js
@@ -0,0 +1,58 @@
+// See [Example](https://raw.githubusercontent.com/crossutility/Quantumult-X/master/resource-parser.js).
+
+const Rule = {
+  domain: (text) => ({ type: "domain", text }),
+  domainSuffix: (text) => ({ type: "domain-suffix", text }),
+
+  from_domainlist_rule: (s) => {
+    let [text, type] = s.trim().split(":").reverse();
+    switch (type) {
+      case "full":
+        return Rule.domain(text);
+      case "domain":
+      case undefined:
+        return Rule.domainSuffix(text);
+      case "include":
+      default:
+        var msg = `\`${type}:...\` is not supported` + " " +
+          `${type === "include" ? "" : "yet"}` + ".";
+        $notify("Invalid filter", "domainlist filter", msg);
+        $done({ error: msg });
+        throw new Error(msg);
+    }
+  },
+  to_qx_rule: (rule) => {
+    switch (rule.type) {
+      case "domain":
+        return `host,${rule.text},proxy`;
+      case "domain-suffix":
+        return `host-suffix,${rule.text},proxy`;
+    }
+  },
+}
+
+var content = String($resource.content)
+var url = new URL($resource.link)
+
+if (url.searchParams.get("format") === "v2ray") {
+  let add_via = url.searchParams.get("add_via") !== null;
+
+  const filterline = (line) => {
+    var line = line.trim();
+    return !(line.startsWith("#") || line === "");
+  }
+  const mapline = (line, add_via) => {
+    return Rule.to_qx_rule(Rule.from_domainlist_rule(line)) +
+      (add_via ? ",via-interface=%TUN%" : "")
+  }
+
+  var result = content.split("\n")
+    .filter(line => filterline(line))
+    .map(line => mapline(line, add_via))
+    .join("\n")
+
+  $done({ content: result })
+} else {
+  // Unmodified
+  $done({content: content})
+}
